b0adc82bed844c95acd656d3372c66e14f37ea62
Toshiki <t_6_ginkgonut_18_i@yahoo.co.jp>
2017-06-21 15:58:02 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module SampleApp
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
